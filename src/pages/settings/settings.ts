5447820c94aba0f74e6425f2f11d00e4486b5340
Unknown <modestemeritz@gmail.com>
2019-02-05 12:15:46 +0000

    FINAL
    Pour la finalisation de l'activiter

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { AlertController, MenuController } from 'ionic-angular';
+import { MenuController } from 'ionic-angular';
 
 @Component({
   selector: 'page-settings',
@@ -7,25 +7,9 @@ import { AlertController, MenuController } from 'ionic-angular';
 })
 export class SettingsPage {
 
-  constructor(private alertCtrl: AlertController, private menuCtrl: MenuController) {}
+  constructor( private menuCtrl: MenuController) {}
 
-  onToggleLights() {
-    let alert = this.alertCtrl.create({
-      title: 'Êtes-vous certain(e) de vouloir continuer ?',
-      subTitle: 'Cette action allumera ou éteindra toutes les lumières de la maison !',
-      buttons: [
-        {
-          text: 'Annuler',
-          role: 'cancel'
-        },
-        {
-          text: 'Confirmer',
-          handler: () => console.log('Confirmé !')
-        }
-      ]
-    });
-    alert.present();
-  }
+  
 
   onToggleMenu() {
     this.menuCtrl.open();
